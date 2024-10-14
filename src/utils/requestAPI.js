2b54f6032c03c3a79d42b15c0c8c53fed1421f18
tran quoc dat <tranquocdat30122004@gmail.com>
2024-10-14 09:10:47 +0700

    update 89Test v2

--- src/utils/requestAPI.js
+++ src/utils/requestAPI.js
@@ -1,5 +1,5 @@
 // const API_DOMAIN="https://eight9test.onrender.com/";
-const API_DOMAIN="http://localhost:8080/";
+const API_DOMAIN="https://eight9test-backend.onrender.com/";
 
 export const get=async(path)=>{
     const response=await fetch(API_DOMAIN+path);
